29ab809e2ee8ccd729b3a8fc26a89749f3835b9c
Shreyas Raorane <raorane@seas.upenn.edu>
2025-01-14 17:26:09 -0500

    launch file added to be checked on linuc

---
+++ ros2_tf2_ws/src/learning_tf2/launch/learning_tf2_launch_1.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher.py',
+            name='static_transform_publisher',
+            arguments=['0', '0', '1', '0', '0', '0', 'world', 'talk']
+        )
+    ])
